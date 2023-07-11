117efa48058143b99331f092a99c15d512da2aaf
bkarimii <behrouzkarimi1993@gmail.com>
2023-07-11 12:42:51 +0100

    crating signup button and form

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     let slideIndex = 0;
     const slides = document.getElementsByClassName("slide");
     const imageSources = [
-        "https://miro.medium.com/v2/resize:fit:840/1*RJMxLdTHqVBSijKmOO5MAg.jpeg",
-        "https://www.freecodecamp.org/news/content/images/2021/06/Ekran-Resmi-2019-11-18-18.08.13.png",
-        "https://blog.logrocket.com/wp-content/uploads/2021/04/handling-dispatching-events-nodejs.png",
-        "https://repository-images.githubusercontent.com/335476519/61ef634e-0b5f-4d27-9fb6-c64d526c595c",
-        "https://eachscape.com/wp-content/uploads/2016/07/JScript.png",
-        "https://www.lambdatest.com/blog/wp-content/uploads/2018/11/JPG-2.jpg"
-      ];
+      "https://miro.medium.com/v2/resize:fit:840/1*RJMxLdTHqVBSijKmOO5MAg.jpeg",
+      "https://www.freecodecamp.org/news/content/images/2021/06/Ekran-Resmi-2019-11-18-18.08.13.png",
+      "https://blog.logrocket.com/wp-content/uploads/2021/04/handling-dispatching-events-nodejs.png",
+      "https://repository-images.githubusercontent.com/335476519/61ef634e-0b5f-4d27-9fb6-c64d526c595c",
+      "https://eachscape.com/wp-content/uploads/2016/07/JScript.png",
+      "https://www.lambdatest.com/blog/wp-content/uploads/2018/11/JPG-2.jpg"
+    ];
   
     const prevButton = document.getElementById("prevButton");
     const nextButton = document.getElementById("nextButton");
@@ -41,5 +41,21 @@ document.addEventListener("DOMContentLoaded", function() {
   
     // Show the initial set of images
     showSlide();
+  
+    // Signup form
+    const signupForm = document.getElementById("signupForm");
+    let signupSuccess = false;
+  
+    signupForm.addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent form submission
+  
+      // Simulate successful signup
+      signupSuccess = true;
+  
+      if (signupSuccess) {
+        alert("Signup successful! Welcome to BlaBla.com");
+        signupForm.reset(); // Reset the form fields
+      }
+    });
   });
   
