9aacfd762e75ea4dcdcc7fe1e9e6e51105bffa79
bkarimii <behrouzkarimi1993@gmail.com>
2023-07-11 11:02:31 +0100

    create carousel by JS
    creating carousel image list loot though them

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
 document.addEventListener("DOMContentLoaded", function() {
     let slideIndex = 0;
     const slides = document.getElementsByClassName("slide");
-    const imageSources = ["C:\Users\bkari\OneDrive\Desktop\personal_webpage\Personal_webpage\Carousel",
-     "image2.jpg", "image3.jpg"];
+    const imageSources = [
+        "https://miro.medium.com/v2/resize:fit:840/1*RJMxLdTHqVBSijKmOO5MAg.jpeg",
+        "https://www.freecodecamp.org/news/content/images/2021/06/Ekran-Resmi-2019-11-18-18.08.13.png",
+        "https://blog.logrocket.com/wp-content/uploads/2021/04/handling-dispatching-events-nodejs.png",
+        "https://repository-images.githubusercontent.com/335476519/61ef634e-0b5f-4d27-9fb6-c64d526c595c",
+        "https://eachscape.com/wp-content/uploads/2016/07/JScript.png",
+        "https://www.lambdatest.com/blog/wp-content/uploads/2018/11/JPG-2.jpg"
+      ];
+  
+    const prevButton = document.getElementById("prevButton");
+    const nextButton = document.getElementById("nextButton");
   
     function showSlide() {
-      // Set the src attribute of the current slide
-      slides[slideIndex].src = imageSources[slideIndex];
+      for (let i = 0; i < 3; i++) {
+        const imgIndex = (slideIndex + i) % imageSources.length;
+        slides[i].src = imageSources[imgIndex];
+      }
+    }
+  
+    function prevSlide() {
+      slideIndex--;
+      if (slideIndex < 0) {
+        slideIndex = imageSources.length - 1;
+      }
+      showSlide();
     }
   
     function nextSlide() {
       slideIndex++;
-      if (slideIndex >= slides.length) {
+      if (slideIndex >= imageSources.length) {
         slideIndex = 0;
       }
       showSlide();
     }
   
-    // Automatically move to the next slide every 3 seconds
-    setInterval(nextSlide, 2000);
+    prevButton.addEventListener("click", prevSlide);
+    nextButton.addEventListener("click", nextSlide);
   
-    // Show the initial slide
+    // Show the initial set of images
     showSlide();
   });
   
